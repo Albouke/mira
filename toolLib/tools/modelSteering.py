13cd9e75afb24eb9101c30188b671b36d76b137a
Albouke <2455065455@qq.com>
2024-12-18 17:54:17 +0800

    Update modelSteering.py

--- toolLib/tools/modelSteering.py
+++ toolLib/tools/modelSteering.py
@@ -5,34 +5,12 @@ from ..tool_configs import ToolInfo, ToolRegistry
 呼叫专家模型
 进行连续对话
 调用系统行为
-"""
+
 class Steering(ToolInfo):
-  def __init__(self):
-    super().__init__()
-    self.NAME = "Steer"
-    self.DESCRIPTION = "Multiply two numbers together"
-    self.PARAMETERS = {
-      "type": "OBJECT",
-      "properties": {
-        "number1": {
-          "type": "NUMBER",
-          "description": "The first number to multiply",
-        },
-        "number2": {
-          "type": "NUMBER",
-          "description": "The second number to multiply",
-        },
-      },
-      "required": ["number1", "number2"]
+没写完
 
-    }
 
-  @staticmethod
-  def execute(number1: float, number2: float) -> float:
-    """执行乘法运算"""
-    print("code11")
-    return number1 * number2
-
+"""
 
 # 注册工具
 ToolRegistry.register_tool(Steering)
